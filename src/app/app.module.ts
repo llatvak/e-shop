8b1f01b5e946ed3edc65111865fc0c5aec9abeeb
Lauri Latva-Kyyny <lauri.latva-kyyny@outlook.com>
2020-05-04 15:34:21 +0300

    Create new component shop-cart.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,11 +7,13 @@ import { NavbarComponent } from './navbar/navbar.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { ShopCartComponent } from './shop-cart/shop-cart.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    ShopCartComponent
   ],
   imports: [
     BrowserModule,
