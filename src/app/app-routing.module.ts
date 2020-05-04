1365e7a0d2ac820f82ca1d5c52af6c86e61ee9e2
Lauri Latva-Kyyny <lauri.latva-kyyny@outlook.com>
2020-05-04 18:18:45 +0300

    Add routing to navbar button.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,19 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { ShopCartComponent } from './shop-cart/shop-cart.component';
+import { SearchResultComponent } from './search-result/search-result.component';
+import { AppComponent } from './app.component';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'login', component: LoginComponent },
+  { path: 'shop-cart', component: ShopCartComponent },
+  { path: 'search-result', component: SearchResultComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
+
 export class AppRoutingModule { }
