5cd4505a7f35d76d2bb74d39322899162834f38c
Lauri Latva-Kyyny <lauri.latva-kyyny@outlook.com>
2020-05-05 13:57:51 +0300

    Add one shop item to front page.

--- src/app/shop-item/shop-item.component.ts
+++ src/app/shop-item/shop-item.component.ts
@@ -7,6 +7,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ShopItemComponent implements OnInit {
 
+  productTitle: string;
+  productImageUrl: string;
+
   constructor() { }
 
   ngOnInit(): void {
