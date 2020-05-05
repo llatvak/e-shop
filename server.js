909e3d0658ab6fdc7e1fd1ad4de63c464c1ea5c4
Lauri Latva-Kyyny <lauri.latva-kyyny@outlook.com>
2020-05-05 20:03:05 +0300

    Fix error when deploying.

--- server.js
+++ server.js
@@ -1,7 +1,11 @@
 const express = require('express');
+
 const app = express();
+
 app.use(express.static('./dist/eshop'));
+
 app.get('/*', (req, res) =>
     res.sendFile('index.html', {root: 'dist/eshop/'}),
 );
+
 app.listen(process.env.PORT || 8080);
