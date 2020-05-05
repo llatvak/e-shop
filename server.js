8345114e684d0d3cc4d6b3bf5d3a28b56997db4f
Lauri Latva-Kyyny <lauri.latva-kyyny@outlook.com>
2020-05-05 20:19:58 +0300

    Fix file error.

--- server.js
+++ server.js
@@ -2,10 +2,10 @@ const express = require('express');
 
 const app = express();
 
-app.use(express.static('./dist/eshop'));
+app.use(express.static('./dist/Eshop'));
 
-app.get('/*', (req, res) =>
-    res.sendFile('index.html', {root: 'dist/eshop/'}),
+app.get('*', (req, res) =>
+    res.sendFile('index.html', {root: 'dist/Eshop/'}),
 );
 
 app.listen(process.env.PORT || 8080);
