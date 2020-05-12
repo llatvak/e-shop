47afbaac32a8dc8e94e55cbe2b519a973f7d8458
Lauri Latva-Kyyny <lauri.latva-kyyny@outlook.com>
2020-05-12 12:59:12 +0300

    Fix authenticate to post.

--- routes/users.js
+++ routes/users.js
@@ -7,7 +7,7 @@ router.get('/register', (req, res, next) => {
 });
 
 // Authenticate
-router.get('/authenticate', (req, res, next) => {
+router.post('/authenticate', (req, res, next) => {
     res.send('AUTHENTICATE');
 });
 
