f97a2ff804db7194a25e97c0f26512f9920edd82
Lauri Latva-Kyyny <lauri.latva-kyyny@outlook.com>
2020-05-12 13:00:22 +0300

    Change routes.

--- routes/users.js
+++ routes/users.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 // Register
-router.get('/register', (req, res, next) => {
+router.post('/register', (req, res, next) => {
     res.send('REGISTER');
 });
 
@@ -14,11 +14,6 @@ router.post('/authenticate', (req, res, next) => {
 // Profile
 router.get('/profile', (req, res, next) => {
     res.send('PROFILE');
-});
-
-// Validate
-router.get('/validate', (req, res, next) => {
-    res.send('VALIDATE');
 });
 
 module.exports = router;
