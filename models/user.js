8a8d4567ef2b056e5042274da0be6598a72d07ec
Lauri Latva-Kyyny <lauri.latva-kyyny@outlook.com>
2020-05-12 13:07:19 +0300

    Add functions for getting users.

--- models/user.js
+++ models/user.js
@@ -15,3 +15,14 @@ const UserSchema = mongoose.Schema({
 });
 
 const User = module.exports = mongoose.model('User', UserSchema);
+
+// Get one user by id
+module.exports.getUserById = function(id, callback) {
+    User.findById(id, callback);
+}
+
+// Get one user by username
+module.exports.getUserByUsername = function(username, callback) {
+    const query = {username: username}
+    User.findOne(query, callback);
+}
