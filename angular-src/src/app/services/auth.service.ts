6068babf85a1e8c2c11b2785d271c856e158e453
Lauri Latva-Kyyny <lauri.latva-kyyny@outlook.com>
2020-05-12 20:39:39 +0300

    Create function for user registration.

--- angular-src/src/app/services/auth.service.ts
+++ angular-src/src/app/services/auth.service.ts
@@ -1,10 +1,23 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { map } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  constructor() { }
+  authToken: any;
+  user: any;
+
+  constructor(private http: HttpClient) { }
+
+  registerUser(user) {
+    const headers = new HttpHeaders();
+    headers.append('Content-Type', 'application/json');
+    return this.http.post('http://localhost:3000/users/register', user, {headers: headers})
+      .pipe(
+        map((res: Response) => res.json())
+      );
+  }
 }
