d99bbfc029dc1dab05eb6b206b6b61155d50ccbc
Lauri Latva-Kyyny <lauri.latva-kyyny@outlook.com>
2020-05-12 12:48:54 +0300

    Add routes.

--- routes/users.js
+++ routes/users.js
@@ -6,4 +6,14 @@ router.get('/register', (req, res, next) => {
     res.send('REGISTER');
 });
 
+// Authenticate
+router.get('/authenticate', (req, res, next) => {
+    res.send('AUTHENTICATE');
+});
+
+// Profile
+router.get('/profile', (req, res, next) => {
+    res.send('PROFILE');
+});
+
 module.exports = router;
