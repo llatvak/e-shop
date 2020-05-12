b979c693fe42533e4766bf519760874925363017
Lauri Latva-Kyyny <lauri.latva-kyyny@outlook.com>
2020-05-12 12:13:36 +0300

    Add configurations.

--- app.js
+++ app.js
@@ -7,14 +7,25 @@ const mongoose = require('mongoose');
 
 const app = express();
 
+const users = require('./routes/users');
+
+// Port number
 const port = 3000;
 
+// CORS
 app.use(cors());
 
+// Body Parser
+app.use(bodyParser.json());
+
+app.user('/users', users);
+
+// Index route
 app.get('/', (req, res) => {
     res.send('Invalid endpoint');
 });
 
+// Server start
 app.listen(port, () => {
     console.log('Server started on port ' + port);
 });
