380f3ea2bf14a1efa4019b7d1194c1814c6dca53
Lauri Latva-Kyyny <lauri.latva-kyyny@outlook.com>
2020-05-12 19:20:28 +0300

    Move components under own dir.

--- angular-src/src/app/app-routing.module.ts
+++ angular-src/src/app/app-routing.module.ts
@@ -1,10 +1,10 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { LoginComponent } from './login/login.component';
-import { ShopCartComponent } from './shop-cart/shop-cart.component';
-import { SearchResultComponent } from './search-result/search-result.component';
-import { ShopItemComponent } from './shop-item/shop-item.component';
-import { ProductDetailsComponent } from './product-details/product-details.component';
+import { LoginComponent } from './components/login/login.component';
+import { ShopCartComponent } from './components/shop-cart/shop-cart.component';
+import { SearchResultComponent } from './components/search-result/search-result.component';
+import { ShopItemComponent } from './components/shop-item/shop-item.component';
+import { ProductDetailsComponent } from './components/product-details/product-details.component';
 
 const routes: Routes = [
   { path: 'login', component: LoginComponent },
--- angular-src/src/app/app.module.ts
+++ angular-src/src/app/app.module.ts
@@ -5,16 +5,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
-import { ShopCartComponent } from './shop-cart/shop-cart.component';
-import { FeaturedItemsComponent } from './featured-items/featured-items.component';
-import { ShopItemComponent } from './shop-item/shop-item.component';
-import { FooterComponent } from './footer/footer.component';
-import { SearchResultComponent } from './search-result/search-result.component';
-import { LoginComponent } from './login/login.component';
+import { ShopCartComponent } from './components/shop-cart/shop-cart.component';
+import { FeaturedItemsComponent } from './components/featured-items/featured-items.component';
+import { ShopItemComponent } from './components/shop-item/shop-item.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { SearchResultComponent } from './components/search-result/search-result.component';
+import { LoginComponent } from './components/login/login.component';
 import { MaterialModule } from './material.module';
-import { MainNavComponent } from './main-nav/main-nav.component';
+import { MainNavComponent } from './components/main-nav/main-nav.component';
 import { ReactiveFormsModule } from '@angular/forms';
-import { ProductDetailsComponent } from './product-details/product-details.component';
+import { ProductDetailsComponent } from './components/product-details/product-details.component';
 
 @NgModule({
   declarations: [
