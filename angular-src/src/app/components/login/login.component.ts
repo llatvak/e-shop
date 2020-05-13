dd71cf975b5744d36af03ee4733e7f8b10df88f6
Lauri Latva-Kyyny <lauri.latva-kyyny@outlook.com>
2020-05-13 12:20:32 +0300

    Add login button functionality and store token.

--- angular-src/src/app/components/login/login.component.ts
+++ angular-src/src/app/components/login/login.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { AuthService } from '../../services/auth.service';
+import { Router } from '@angular/router';
+
 
 @Component({
   selector: 'app-login',
@@ -10,8 +13,12 @@ export class LoginComponent implements OnInit, OnDestroy {
 
   loginForm: FormGroup;
   emailRegx = /^(([^<>+()\[\]\\.,;:\s@"-#$%&=]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,3}))$/;
-
-  constructor(private formBuilder: FormBuilder) { }
+  userResponse: any = {};
+  constructor(
+    private formBuilder: FormBuilder,
+    private authService: AuthService,
+    private router: Router
+  ) { }
 
   ngOnInit(): void {
     this.loginForm = this.formBuilder.group({
@@ -21,9 +28,25 @@ export class LoginComponent implements OnInit, OnDestroy {
   }
 
   submit() {
+    // On form invalid
     if (!this.loginForm.valid) {
       return;
     }
+    // On form valid
+    const user = this.loginForm.value;
+    this.authService.autheticateUser(user).subscribe(data => {
+      this.userResponse = data;
+      if (this.userResponse.success) {
+        this.authService.storeUserData(this.userResponse.token, this.userResponse.user);
+        console.log('On successful login');
+        this.router.navigate(['/dashboard']);
+      } else {
+        // If login fails navigate back go login page
+        console.log('On error login');
+        this.router.navigate(['/login']);
+      }
+    });
+
     console.log(this.loginForm.value);
   }
 
--- angular-src/src/app/services/auth.service.ts
+++ angular-src/src/app/services/auth.service.ts
@@ -1,6 +1,5 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
-import { map } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -12,9 +11,26 @@ export class AuthService {
 
   constructor(private http: HttpClient) { }
 
+  // Register user to database
   registerUser(user) {
     const headers = new HttpHeaders();
     headers.append('Content-Type', 'application/json');
     return this.http.post('http://localhost:3000/users/register', user, {headers});
+  }
+
+  // Authenticate user and fetch token if successfull
+  autheticateUser(user) {
+    const headers = new HttpHeaders();
+    headers.append('Content-Type', 'application/json');
+    return this.http.post('http://localhost:3000/users/authenticate', user, {headers});
+  }
+
+  storeUserData(token, user) {
+    // Angular-JWT look for id_token key when validating token
+    localStorage.setItem('id_token', token);
+    // Parse user from JSON format to string so it can be saved to local storage
+    localStorage.setItem('user', JSON.stringify(user));
+    this.authToken = token;
+    this.user = user;
   }
 }
