d270b344c94654934e13c2cb0c6986bb0d9e4283
Lauri Latva-Kyyny <lauri.latva-kyyny@outlook.com>
2020-05-17 15:09:40 +0300

    Add comments.

--- angular-src/src/app/components/product-details/product-details.component.ts
+++ angular-src/src/app/components/product-details/product-details.component.ts
@@ -28,16 +28,18 @@ export class ProductDetailsComponent implements OnInit {
   @ViewChild('autosize') autosize: CdkTextareaAutosize;
 
   ngOnInit(): void {
+    // Get id from url as parameter to fetch item details even on refresh or
+    // manual url
     this.route.params.subscribe(params => {
       this.itemId = params['id'];
-      console.log(this.itemId);
+      // Fetch item by id
       this.itemService.getItem(this.itemId)
         .subscribe( data => {
-          console.log(data);
           this.item = data;
           this.productDetailForm = this.formBuilder.group({
             size: [null, [Validators.required]]
           });
+        // In case of bad request navigate back to home page
       }, err => {
         this.router.navigate(['/']);
       });
