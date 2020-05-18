ae0adbb7529c29ccfd4e17fdf47c23224543c220
Lauri Latva-Kyyny <lauri.latva-kyyny@outlook.com>
2020-05-18 21:41:05 +0300

    Add item to cart.

--- angular-src/src/app/components/main-nav/main-nav.component.ts
+++ angular-src/src/app/components/main-nav/main-nav.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, DoCheck } from '@angular/core';
 import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
 import { Observable } from 'rxjs';
 import { map, shareReplay } from 'rxjs/operators';
@@ -11,7 +11,7 @@ import { AuthService } from '../../services/auth.service';
   templateUrl: './main-nav.component.html',
   styleUrls: ['./main-nav.component.css']
 })
-export class MainNavComponent implements OnInit {
+export class MainNavComponent implements OnInit, DoCheck {
   faShoppingCart = faShoppingCart;
   faUserCircle = faUserCircle;
   faSearch = faSearch;
@@ -36,6 +36,9 @@ export class MainNavComponent implements OnInit {
   ) {}
 
   ngOnInit(): void {
+  }
+
+  ngDoCheck(): void {
     const cartItems = localStorage.getItem('cart').split(',');
     this.shopCartItemCount = cartItems.length;
   }
