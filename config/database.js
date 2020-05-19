3728e7bd92a098febd4b00568640dd0c03a2958f
Lauri Latva-Kyyny <lauri.latva-kyyny@outlook.com>
2020-05-19 15:23:47 +0300

    Switch database url to cloud.

--- config/database.js
+++ config/database.js
@@ -1,4 +1,4 @@
 module.exports = {
-    database: 'mongodb://localhost:27017/eshop',
+    database: 'mongodb+srv://late_228:late_3321@llcluster-7efv6.mongodb.net/test?retryWrites=true&w=majority',
     secret: 'mysecret'
 }
